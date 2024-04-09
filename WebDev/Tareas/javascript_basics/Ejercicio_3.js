802cbb9bf903333943b5f65749b8c1c6a5931a1d
Miguel Soria <a01028033@tec.mx>
2024-04-09 10:12:45 -0600

    added ejerccio_3

---
+++ WebDev/Tareas/javascript_basics/Ejercicio_3.js
@@ -0,0 +1,33 @@
+// Escribe dos funciones: la primera que invierta un arreglo de números y regrese un nuevo arreglo con el resultado; la segunda que modifique el mismo arreglo que se pasa como argumento. No se permite usar la función integrada 'reverse'.
+
+"use strict";
+// Swap function for more readable code
+function swap(arr, i, j) {
+  //swaps two elements in an array
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function revererseArray(arr) {
+  // returns a new array with the elements of the original array in reverse order
+  let reversedArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArr.push(arr[i]);
+  }
+  return reversedArr;
+}
+
+let arr = [1, 2, 3, 4, 5];
+console.log(revererseArray(arr));
+
+function revertArraySelf(arr) {
+  // Reverses an array within itself
+  for (let i = 0; i < arr.length / 2; i++) {
+    swap(arr, i, arr.length - 1 - i);
+  }
+  return arr;
+}
+
+
+console.log(revertArraySelf(arr));
