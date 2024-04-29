6d23e8ba9abe29230d970bab307cc8b8af44c803
Miguel Soria <a01028033@tec.mx>
2024-04-28 23:35:51 -0600

    Finished update endpoint for updating current cards

--- WebDev/Tareas/api_basics_tcg/app.js
+++ WebDev/Tareas/api_basics_tcg/app.js
@@ -5,6 +5,9 @@
 import express from 'express';
 const port = 5000;
 const app = express();
+// Card template for the wanted parameters of the cards in the game
+let card_template = ["id", "name", "type_id", "type_name", "hp", "speed", "speed_cost", "atk", "def"];
+
 let card_list = [
 {
   "id": 1, 
@@ -52,18 +55,23 @@ app.get("/lookup/:id", (req, res) => {
 
 app.post("/add_card", (req, res) => {
   const card = req.body;
+
+  for (let i = 0; i < card_template.length; i++) {
+    if (!card.hasOwnProperty(card_template[i])) {
+      res.status(200).send(`Card is missing ${card_template[i]}.`);
+      return;
+    }
+  }
   if (card_list.find((c) => c.id === card.id)) {
     res.status(200).send("Card already exists.");
-  } else {
+  } else { 
     card_list.push(card);
-    res.json(card_list);
     res.status(200).send("Card added into card list.");
-  }
-});
+}});
 
 app.delete("/delete_card/:id", (req, res) => {
   const id = req.params.id;
-  const card = card_list.find((card) => card.id === id);
+  const card = card_list.find((card) => card.id == id);
   if (card) {
     card_list = card_list.filter((card) => card.id !== id);
     res.json(card_list);
@@ -71,6 +79,32 @@ app.delete("/delete_card/:id", (req, res) => {
     res.status(200).send("Card not found in the card list.");
   }
 });
+
+app.put("/update_card/:id", (req, res) => {
+  const id = req.params.id;
+  // Get the fields to update
+  const updatedFields = req.body;
+
+  const cardIndex = card_list.findIndex((card) => card.id == id);
+  if (!cardIndex) {
+    res.status(200).send("Card not found in the card list.");
+    return;
+  }
+
+  if (updatedFields.id) {
+    // ID protection
+    res.status(200).send("Cannot update card id.");
+    return;
+  }
+
+  const existingCard = card_list[cardIndex];
+  for (const field in updatedFields) {
+    // Update only the fields that are present in the card template
+    existingCard[field] = updatedFields[field];
+  }
+  res.status(200).send("Card updated successfully.");
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
