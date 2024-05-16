da6e2982a9f816e6e05210846155809c79e9e064
Miguel Soria <a01028033@tec.mx>
2024-05-16 13:27:05 -0600

    might fail

---
+++ Videogames/Pong/Assets/Scripts/APIConnection.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class APIConnection : MonoBehaviour
+{
+    [SerializeField] string url;
+    [SerializeField] string getEndpoint;
+
+
+    SimonCotroller controller;
+
+    void Start() {
+        controller = GetComponent<SimonCotroller>();
+    }
+
+    public void GetData() {
+        //Debug.Log("GetData Access");
+        StartCoroutine(RequestGet(url + getEndpoint));
+            }
+
+    IEnumerator RequestGet(string url) {
+        // Prepare the request object
+        using(UnityWebRequest www = UnityWebRequest.Get(url)) {
+            // Make the requuest and wait for it to respond
+            yield return www.SendWebRequest();
+            Debug.Log("Request sent");
+
+            // Validate response
+            if(www.result!=UnityWebRequest.Result.Success) {
+                Debug.Log("Request Failed" + www.error);
+            } else {
+                string result = www.downloadHandler.text;
+                Debug.Log("The response was: " + result);
+                controller.apiData = result;
+                controller.PrepareButtons();
+            }
+        }
+    }   
+}
