83cde017024ccf64d266078fc86a63d86429c382
Miguel Soria <a01028033@tec.mx>
2024-05-17 18:59:50 -0600

    Added html file for html cheatsheet as well as the necessary .js file and package settings necessary for the node server

---
+++ WebDev/Tareas/html-cheatsheet/app.js
@@ -0,0 +1,22 @@
+// Miguel Soria A01028033 22/04/2024
+"use strict"
+
+
+import express from 'express';
+import fs from "fs";
+
+const port = 3000;
+const app = express();
+let card_list = [];
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const file = fs.readFileSync("public/html/html_cheat_sheet.html", "utf-8");
+  res.status(200).send(file);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
