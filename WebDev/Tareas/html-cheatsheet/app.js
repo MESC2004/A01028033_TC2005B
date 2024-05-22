9bc0069ef5b8c4f4fbecfb2c9b58b6c1a789570a
Miguel Soria <a01028033@tec.mx>
2024-05-21 18:00:03 -0600

    added endpoint in app.js to acces css or html cheatsheet through /css or /html, added css and html files for css cheatsheet

--- WebDev/Tareas/html-cheatsheet/app.js
+++ WebDev/Tareas/html-cheatsheet/app.js
@@ -11,11 +11,15 @@ let card_list = [];
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/html", (req, res) => {
   const file = fs.readFileSync("public/html/html_cheat_sheet.html", "utf-8");
   res.status(200).send(file);
 });
 
+app.get("/css", (req, res) => {
+  const file = fs.readFileSync("public/html/css_cheat_sheet.html", "utf-8");
+  res.status(200).send(file);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
